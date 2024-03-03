986748a202f4f15e3b24835fc442f44ced73a8a2
Amir Eliz <tripl3n2@gmail.com>
2024-03-03 09:27:45 +0200

    tests

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
 			exec_mode: 'cluster',
 			instances: '1', // Or a number of instances
 			script: 'pnpm start -p 3333',
-			args: 'start',
 			autorestart: true,
 			watch: true,
 			ignore_watch: ['node_modules', 'logs', '.git'], // Add more directories or files as needed
