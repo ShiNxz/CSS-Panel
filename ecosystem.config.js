fa43cd57e7218b55106fd81a75e3b63db4e74915
Amir Eliz <tripl3n2@gmail.com>
2024-03-03 07:37:20 +0200

    tests

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,11 @@ module.exports = {
 			name: 'CSSPanel',
 			exec_mode: 'cluster',
 			instances: '1', // Or a number of instances
-			script: 'node_modules/next/dist/bin/next',
+			script: 'npm start',
 			args: 'start',
 			autorestart: true,
+			watch: true,
+			ignore_watch: ['node_modules', 'logs', '.git'], // Add more directories or files as needed
 		},
 	],
 }
