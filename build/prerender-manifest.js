abbf305eb857e71b2ac8d2eb8b417a73620d5b82
Amir Eliz <tripl3n2@gmail.com>
2024-03-04 07:10:20 +0200

    New Welcome Screen!

--- build/prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"notFoundRoutes\":[],\"preview\":{\"previewModeId\":\"33ddb81cd1c152450a688a3bf21c9e0c\",\"previewModeSigningKey\":\"0b1174e4d6ac035591393e1dc701239205cce558e897980c92efd4be6ce98ab0\",\"previewModeEncryptionKey\":\"7d4108d7b1c5a44b91a33652190d52adf6bb89b022e4556a29dab18849509a17\"}}"
--- build/server/server-reference-manifest.js
+++ build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"sfh3jnlUAjp9VH5rsCRfB7SIy6RWJBHd4Y6HnTs31Dg=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M/rW62GRDKAoNYT2vrKillRoMSXv6BVba4a9SyvKOwM=\"\n}"
