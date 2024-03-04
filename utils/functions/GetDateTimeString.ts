3f7ccb50bc621d43ab9dd40396b0e1fe1d39b434
Amir Eliz <tripl3n2@gmail.com>
2024-03-04 07:55:28 +0200

    New Welcome Screen!

--- utils/functions/GetDateTimeString.ts
+++
@@ -1,7 +0,0 @@
-const GetDateTimeString = (date?: Date | string | number) => {
-	const newDate = date ? new Date(date) : new Date()
-
-	return newDate.toLocaleDateString() + ' - ' + newDate.toLocaleTimeString()
-}
-
-export default GetDateTimeString
