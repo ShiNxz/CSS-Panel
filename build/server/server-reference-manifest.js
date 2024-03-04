79095104730027c001a64c921bd733347013a205
Amir Eliz <tripl3n2@gmail.com>
2024-03-04 08:13:13 +0200

    0.9.8

--- build/server/server-reference-manifest.js
+++ build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"PTT3M/I9ycbZ8SuiISlutJ9xfcVny7oEHTlPa3oSL1I=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"gTHTzAPUd0C5cAC+C/+WR5tZs+eg469mXj6F3y8QZao=\"}"
